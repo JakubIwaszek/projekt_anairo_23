c7f2bb51b4b04c1508217dde8ebf8e0824fdf7dc
unknown <s223603@student.uek.krakow.pl>
2023-12-08 19:53:03 +0100

    working on design pages - Login/Register/AddNewPostPage views

---
+++ projekt_anairo_23/AddNewPostPageView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewPostPageView.swift
+//  projekt_anairo_23
+//
+//  Created by Przemysław Szwajcowski on 08/12/2023.
+//
+
+import SwiftUI
+
+struct AddNewPostPageView: View {
+    var body: some View {
+        Text("Add new post")
+    }
+}
+
+#Preview {
+    AddNewPostPageView()
+}
---
+++ projekt_anairo_23/Supporting Files/Utilities.swift
@@ -0,0 +1,26 @@
+//
+//  Utilities.swift
+//  projekt_anairo_23
+//
+//  Created by Przemysław Szwajcowski on 24/11/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    public static var lightGray: Color = {
+        return Color(.lightGray)
+    }()
+    
+    public static var mainBackgroundColor: Color = {
+        return Color("mainBackgroundColor")
+    }()
+    
+    public static var postBackgroundColor: Color = {
+        return Color("postBackgroundColor")
+    }()
+    
+    public static var loginBackgroundColor: Color = {
+        return Color("loginBackgroundColor")
+    }()
+}
